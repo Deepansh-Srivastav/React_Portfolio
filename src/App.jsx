e9f97f3530265d8eb91411fe6d406c8a2530c811
Deepansh Srivastav <deepanshsrivastav9711@gmail.com>
2024-09-21 21:14:05 +0530

    Editing in the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header"
+import RouterComponent from "./routes/RouterComponent"
 import "./styles/App.css"
 
 const App = () => {
   return (<>
     <Header />
-  </>
+    <RouterComponent/>
+    </>
   )
 }
 
