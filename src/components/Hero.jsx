229f0663a38d62bcb2d5d1769840aea31c270630
DeepanshUnravel <deepansh@gounravel.com>
2025-06-15 17:55:47 +0530

    Chnages Done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,9 @@ import "../styles/Hero.css";
 import "../styles/Common.css";
 import myImage from "../assets/Images/myImage.png";
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { Typewriter } from 'react-simple-typewriter';
 
 function Hero() {
-
     return (
         <section id="myImage" className="w-100 d-flex justify-content-center align-items-center custom_margin hero_section">
             <div className="d-flex flex-column justify-content-center align-items-center mt-3 hero_section" data-aos="fade-down">
@@ -13,11 +13,22 @@ function Hero() {
                 </div>
 
                 <h2>
-                    Hi <span className="wave">👋🏻</span>, I'm Deepansh Srivastav
+                    Hi <span className="wave">👋🏻</span>, I'm Deepansh{' '}
+                    <span style={{ color: '#00FFAA', fontWeight: 'bold' }}>
+                        <Typewriter
+                            words={['Srivastav']}
+                            loop={0} // 0 = infinite
+                            cursor
+                            cursorStyle="_"
+                            typeSpeed={100}
+                            deleteSpeed={80}
+                            delaySpeed={2000}
+                        />
+                    </span>
                 </h2>
             </div>
         </section>
     );
-};
+}
 
 export default Hero;
