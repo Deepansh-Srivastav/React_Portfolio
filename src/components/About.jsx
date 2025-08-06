02d9c6aad0ba47d4e780bd85585779a050c358b0
DeepanshUnravel <deepansh@gounravel.com>
2025-08-06 22:16:15 +0530

    .

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,18 +10,21 @@ const About = () => {
     <section className="about_section">
       <div className="row">
         <div className="col-12 col-md-6 ">
-          <div className="image_container d-flex flex-row justify-content-center align-items-end" data-aos = "fade-right">
+          <div className="image_container d-flex flex-row justify-content-center align-items-end" data-aos="fade-right">
             <img src={aboutImage} alt="Image Deepansh Srivastav" />
           </div>
         </div>
 
         <div className="col-12 col-md-6">
-          <div className="py-5" data-aos = "fade-left">
+          <div className="py-5" data-aos="fade-left">
             <Heading_Type_1 {...aboutSectionHeadingData} />
             <div className="general_text">
-              <p>Hey there! I&apos;m Deepansh Srivastav, a passionate student and a keen learner specializing in MERN stack development. Driven by curiosity and a dedication to growth, I continuously seek to expand my knowledge and skillset.<br /><br />My mission is to leverage the insights I gain to make a positive impact.
-              <br />
-              Join me on this exciting journey of learning, exploration, and innovation as we uncover new possibilities and embrace growth together.</p>
+              <p>
+                Hello! I&apos;m Deepansh Srivastav, a dedicated Frontend Developer with over 10 months of real-world experience in building responsive and user-centric web applications using React.js. I specialize in creating clean, performant, and accessible interfaces that enhance user experience and bring designs to life.<br /><br />
+                With a strong foundation in modern frontend technologies and a continuous drive to stay updated with industry trends, I strive to craft scalable and maintainable solutions. <br /><br />
+                I&apos;m  passionate about transforming ideas into interactive digital experiences and always eager to collaborate, innovate, and grow in this ever-evolving tech landscape.
+              </p>
+
             </div>
           </div>
         </div>
