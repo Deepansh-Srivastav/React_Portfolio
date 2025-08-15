0bb419ca4bf0548905ce3c26e1eb5552247db9f9
DeepanshUnravel <deepansh@gounravel.com>
2025-08-15 14:02:47 +0530

    Resolving the build error

--- src/data/images.js
+++ src/data/images.js
@@ -1,47 +1,47 @@
 import portfolioLogo from "../assets/Logos/portfolio_logo.png";
-import BitlyzeCeritifcate from "../assets/Certificates/Bitlyze.png"
-import ProDevCertificate from "../assets/Certificates/Prodev.png"
+import BitlyzeCeritifcate from "../assets/Certificates/Bitlyze.png";
+import ProDevCertificate from "../assets/Certificates/Prodev.png";
 import Degree from "../assets/Certificates/Degree.png";
 import Git_Github from "../assets/Certificates/Git-Github.png";
 import Cetpa from "../assets/Certificates/Cetpa.png";
 import Web_dev from "../assets/Certificates/Web-dev.png";
-import nodeIcon from "../assets/Logos/node.png"
-import expressIcon from "../assets/Logos/ex.png"
-import postmanIcon from "../assets/Logos/postman.png"
-import reactIcon from "../assets/Logos/react.png"
-import html from "../assets/Logos/html.png"
-import css from "../assets/Logos/css.png"
-import js from "../assets/Logos/js.png"
-import bs from "../assets/Logos/bs.png"
-import azure from "../assets/Logos/azure.png"
-import react from "../assets/Logos/react.png"
-import git from "../assets/Logos/git.png"
-import github from "../assets/Logos/github.png"
-import mongoIcon from "../assets/Logos/mongo.png"
-import muiIcon from "../assets/Logos/mui.png"
-import zomato from "../assets/Project_Images/Zomato/zomato1.png"
-import zomato2 from "../assets/Project_Images/Zomato/zomato2.png"
-import zomato3 from "../assets/Project_Images/Zomato/zomato3.png"
-import zomato4 from "../assets/Project_Images/Zomato/zomato4.png"
-import Cafe from "../assets/Project_Images/Cafe/cafe1.png"
-import Cafe2 from "../assets/Project_Images/Cafe/cafe2.png"
-import Cafe3 from "../assets/Project_Images/Cafe/cafe3.png"
-import Cafe4 from "../assets/Project_Images/Cafe/cafe4.png"
-import Cafe5 from "../assets/Project_Images/Cafe/cafe5.png"
-import pf from "../assets/Project_Images/Portfolio/pf1.png"
-import pf2 from "../assets/Project_Images/Portfolio/pf2.png"
-import pf3 from "../assets/Project_Images/Portfolio/pf3.png"
-import pf4 from "../assets/Project_Images/Portfolio/pf4.png"
-import SD1 from "../assets/Project_Images/Swiftdrop/SD1.png"
-import SD2 from "../assets/Project_Images/Swiftdrop/SD2.png"
-import hero_image from "../assets/Images/me-green.png"
-import unravel from "../assets/Logos/unravel.png"
-import abesit from "../assets/Logos/abesit.png"
-import bitlyze from "../assets/Logos/bitlyze.png"
-import udemy from "../assets/Logos/udemy.png"
-import cetpaLogo from "../assets/Logos/cetpa.png"
-import experienceImage from "../assets/Images/experience.jpg"
-import educationImage from "../assets/Images/education.jpg"
+import nodeIcon from "../assets/Logos/node.png";
+import expressIcon from "../assets/Logos/ex.png";
+import postmanIcon from "../assets/Logos/postman.png";
+import reactIcon from "../assets/Logos/react.png";
+import html from "../assets/Logos/html.png";
+import css from "../assets/Logos/css.png";
+import js from "../assets/Logos/js.png";
+import bs from "../assets/Logos/bs.png";
+import azure from "../assets/Logos/azure.png";
+import react from "../assets/Logos/react.png";
+import git from "../assets/Logos/git.png";
+import github from "../assets/Logos/github.png";
+import mongoIcon from "../assets/Logos/mongo.png";
+import muiIcon from "../assets/Logos/mui.png";
+import zomato from "../assets/Project_Images/Zomato/zomato1.png";
+import zomato2 from "../assets/Project_Images/Zomato/zomato2.png";
+import zomato3 from "../assets/Project_Images/Zomato/zomato3.png";
+import zomato4 from "../assets/Project_Images/Zomato/zomato4.png";
+import Cafe from "../assets/Project_Images/Cafe/cafe1.png";
+import Cafe2 from "../assets/Project_Images/Cafe/cafe2.png";
+import Cafe3 from "../assets/Project_Images/Cafe/cafe3.png";
+import Cafe4 from "../assets/Project_Images/Cafe/cafe4.png";
+import Cafe5 from "../assets/Project_Images/Cafe/cafe5.png";
+import pf from "../assets/Project_Images/Portfolio/pf1.png";
+import pf2 from "../assets/Project_Images/Portfolio/pf2.png";
+import pf3 from "../assets/Project_Images/Portfolio/pf3.png";
+import pf4 from "../assets/Project_Images/Portfolio/pf4.png";
+import SD1 from "../assets/Project_Images/Swiftdrop/SD1.png";
+import SD2 from "../assets/Project_Images/Swiftdrop/SD2.png";
+import hero_image from "../assets/Images/me-green.png";
+// import unravel from "../assets/Logos/unravel.png";
+import abesit from "../assets/Logos/abesit.png";
+import bitlyze from "../assets/Logos/bitlyze.png";
+import udemy from "../assets/Logos/udemy.png";
+import cetpaLogo from "../assets/Logos/cetpa.png";
+import experienceImage from "../assets/Images/experience.jpg";
+import educationImage from "../assets/Images/education.jpg";
 
 export {
     portfolioLogo,
@@ -81,7 +81,7 @@ export {
     SD1,
     SD2,
     hero_image,
-    unravel,
+    // unravel,
     abesit,
     bitlyze,
     udemy,
